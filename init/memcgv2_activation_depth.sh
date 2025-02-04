SuperiorOS-Violet/system_core

commit 34dae35fe8e318d2d77a72fea4b9176873f5731b

Don't reboot for memcg MaxActivationDepth reduction
19% of 25Q2 Beta unenrollment has been due to reboots. Let's avoid the
one for when experiment server flags change memcg device configuration.
We'll try to use the timestamps of flag rollout/rollback to look at
metrics (and hopefully it's not too noisy).

Bug: 393588812
Test: setprop persist.device_config.mglru_native.lru_gen_config all
Test: setprop persist.device_config.mglru_native.lru_gen_config none
Test: Observe no reboot
Ignore-AOSP-First: 25Q2 Beta Experiment only go/android-memcgv2-exp
Change-Id: I50d9be83756ce73d24636d992fcc528debeea1fd

init/memcgv2_activation_depth.sh
@@ -71,10 +71,9 @@ then
     # We can deactivate memcgs which are deeper than the new depth value, however that would leave
     # behind zombie memcgs which would ruin the metrics produced from this device. The only way to
     # eliminate those zombies is to remove the entire cgroup, which we cannot do without killing
-    # all the contained processes. So the only real option we have is to reboot here.
-    # This should only happen once at the end of the experiment.
-    echo "Rebooting due to decreased memcg v2 MaxActivationDepth"
-    reboot
+    # all the contained processes. So the only real option we have is to reboot here, but that would
+    # look like a random reboot to users. So don't do anything now. Wait until the next reboot for
+    # the new setting to be applied.
 elif [ $max_activation_depth -gt $current_activation_depth ]
 then
     for d in $(seq $max_activation_depth)

commit f20b8c37b30ef4ab8a026e035e0b670eb193eb6e

Fix empty if statement
Add noop to fix:
/system/bin/memcgv2_activation_depth.sh[76]: syntax error: unexpected 'elif'

Bug: 393588812
Fixes: 34dae35fe8 ("Don't reboot for memcg MaxActivationDepth reduction")
Test: /system/bin/memcgv2_activation_depth.sh 3
Test: find /sys/fs/cgroup -name cgroup.controllers -exec grep -q memory {} \; -print
Test: /system/bin/memcgv2_activation_depth.sh 0 (no reboot)
Ignore-AOSP-First: 25Q2 Beta Experiment only go/android-memcgv2-exp
Change-Id: Ia219f7f30c49911dfc347511740db37362f54dbf

init/memcgv2_activation_depth.sh
@@ -74,6 +74,7 @@ then
     # all the contained processes. So the only real option we have is to reboot here, but that would
     # look like a random reboot to users. So don't do anything now. Wait until the next reboot for
     # the new setting to be applied.
+    :
 elif [ $max_activation_depth -gt $current_activation_depth ]
 then
     for d in $(seq $max_activation_depth)